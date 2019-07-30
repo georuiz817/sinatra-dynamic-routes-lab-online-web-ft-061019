1d7dbb0bad57b4442feb6b07f5630d5abea5486d
George Ruiz <georuiz817@gmail.com>
2019-07-30 20:06:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ get '/say/:number/:phrase' do
     "#{Array.new(params[:number].to_i, params[:phrase]).join(", ")}"
   end
 
-get '/say/
-
+get '/say/:word1/:word2/:word3/:word4/:word5' do 
+   '#{params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]'
+end
 end
