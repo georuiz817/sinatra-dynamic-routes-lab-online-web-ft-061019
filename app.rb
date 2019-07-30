c157be227df25491c1436d0052b81a0db3d93fa5
George Ruiz <georuiz817@gmail.com>
2019-07-30 20:10:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
 end
 
 get ':operation/:number1/:number2' do 
-'#{params[:number1].to_i + params[:number2].to_1'
+"#{params[:number1].to_i + params[:number2].to_1"
+
 end
 end
